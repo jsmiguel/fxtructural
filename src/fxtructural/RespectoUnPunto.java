5661fb466fbec23b62bc997c63aa0fa23663f112
JMiguel Garcia <js.miguel@outlook.com>
2014-05-24 10:00:13 -0600

    Moemnto respecto a un punto completo

--- src/fxtructural/RespectoUnPunto.java
+++ src/fxtructural/RespectoUnPunto.java
@@ -23,16 +23,13 @@ import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
-import org.controlsfx.control.Notifications;
-import org.controlsfx.control.action.Action;
-import org.controlsfx.dialog.Dialog;
-import org.controlsfx.dialog.Dialogs;
 
 /**
  *
@@ -42,6 +39,10 @@ public class RespectoUnPunto extends VBox {
     
     final Font metroFont = Font.loadFont(
       Fxtructural.class.getResource("SEGOEUIL.ttf").toExternalForm(), 22);
+    
+    Label lblLambda = new Label();
+    Label lblMomento = new Label();
+    Label lblResultado = new Label();
     
     public VBox RespectoUnPunto(){
          HBox hBox = new HBox();
@@ -83,7 +84,11 @@ public class RespectoUnPunto extends VBox {
         TextField txtFuerza = new TextField();
         txtFuerza.setPromptText("Fuerza");
         
-        
+        VBox vbDetalle = new VBox();
+        vbDetalle.getChildren().addAll(lblLambda, lblMomento, lblResultado);
+        vbDetalle.setPadding(new Insets(15, 0, 15, 0));
+        vbDetalle.setAlignment(Pos.BASELINE_CENTER);
+        vbDetalle.setSpacing(10);
         
         //Calculo respecto a un punto
         Button btnCalcular = new Button("Calcular");
@@ -94,7 +99,7 @@ public class RespectoUnPunto extends VBox {
             DecimalFormatSymbols simbolo=new DecimalFormatSymbols();
             simbolo.setDecimalSeparator('.');
             simbolo.setGroupingSeparator(',');
-            DecimalFormat df = new DecimalFormat("0.00#", simbolo);
+            DecimalFormat df = new DecimalFormat("0.0#", simbolo);
             double producto[] = new double[7];
 
             double x1 = parseDouble(txtPx.getText());
@@ -138,23 +143,15 @@ public class RespectoUnPunto extends VBox {
             double momentoi = (rProducto1i-rProducto2i);
             double momentoj = (rProducto1j-rProducto2j);
             double momentok = (rProducto1k-rProducto2k);
-
+            
+            lblLambda.setText("λ Lambda = "+df.format(lambdax) +","+df.format(lambday)+","+df.format(lambdaz));
+            lblMomento.setText("Momento:");
+            lblResultado.setText(df.format(momentoi)+" i , "+df.format(momentoj)+" j , "+df.format(momentok)+" k");
             System.out.println("El lambda es: "+df.format(lambdax) +","+df.format(lambday)+","+df.format(lambdaz));
             System.out.println("Modulo: "+modulo);
             System.out.println(brazoi+"i + "+brazoj+"j + "+brazok+"k");
             System.out.println(lambdax+"i + "+lambday+"j + "+lambdaz+"k");
-            System.out.println(momentoi+"i + "+momentoj+"j + "+momentok+"k");
-
-            Action response = Dialogs.create()
-                .title("Respuesta")
-                .nativeTitleBar()
-                .masthead("Valor Ingresado")
-                .message("Hola " + x1).showConfirm();
-                if (response == Dialog.Actions.YES) {               
-                  Notifications.create()
-                    .text("Realizado con éxito")
-                    .showInformation();
-                }      
+            System.out.println(momentoi+"i + "+momentoj+"j + "+momentok+"k");    
              
         });
         
@@ -170,7 +167,7 @@ public class RespectoUnPunto extends VBox {
         hBox3.setPadding(new Insets(15, 0, 15, 0));
         hBox3.setSpacing(10);
         
-        vBox.getChildren().addAll(label1,hBox, label2, hBox2, hBox3, btnCalcular);
+        vBox.getChildren().addAll(label1,hBox, label2, hBox2, hBox3, btnCalcular, vbDetalle);
         vBox.setPadding(new Insets(15, 12, 15, 12));
         
         return vBox;
